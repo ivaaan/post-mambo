595decd018280fcd8f811ba7a5e395def93369ae
Ivan Afanasev <diykarelia@gmail.com>
2024-01-16 22:12:16 -0800

    fix: mobile header, menu

--- src/pages/HeaderLogo.jsx
+++ src/pages/HeaderLogo.jsx
@@ -12,7 +12,7 @@ function HeaderLogo() {
 	// 	</div>
 	// ) :
 	return (
-		<div className='fixed w-7/12 z-50 -top-6 -left-6 -right-4'>
+		<div className='fixed w-1/2 lg:w-7/12 xl:w-7/12 2xl:w-7/12 z-50 md:-top-3 md:-left-3 md:-right-2 lg:-top-6 lg:-left-6 lg:-right-4 xl:-top-6 xl:-left-6 xl:-right-4 2xl:-top-6 2xl:-left-6 2xl:-right-4'>
 			<img src='/pm-logo-white.png'></img>
 		</div>
 	);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
 	return (
 		<>
 			<Menu />
-			<div className='w-screen h-screen bg-blue overflow-hidden relative shrink-0 m-0 p-0'>
+			<div className='w-screen h-screen bg-blue overflow-hidden shrink-0 m-0 p-0'>
 				<div className='flex flex-col w-full items-center'>
 					<div className='aspect-video'>
 						<Vimeo
@@ -21,25 +21,28 @@ function Home() {
 							loop='true'
 						/>
 					</div>
-					<div className='w-full m-0 flex justify-center'>
-						{/* <div className='w-full m-0 flex flex-col md:flex-row lg:flex-row xl:flex-row 2xl:flex-row justify-center'> */}
-						<div className='-mt-40 ml-1 w-full md:w-1/3 lg:w-1/3 xl:w-1/3 2xl:w-1/3 rounded-3xl bg-silver text-blue tracking-tight p-8 font-authenticSans90 text-2xl'>
-							<div className=''>
-								<span className='w-1/2'>
-									<img
-										className='w-1/3 float-left mr-4'
-										src='/pm-logo-blue.png'></img>
-								</span>
-								<p className='-mt-5'>
-									is an award-winning Los Angeles and Chicago based
-									post-production company.
-								</p>
-							</div>
-							<span className='mt-4'>
-								We specialize in sound and edit for feature films,
-								documentaries, and commercials.
-							</span>
-							{/* <p>
+				</div>
+			</div>
+
+			<div className='bg-blue z-50 m-0 p-0 flex justify-center'>
+				{/* <div className='w-full m-0 flex flex-col md:flex-row lg:flex-row xl:flex-row 2xl:flex-row justify-center'> */}
+				<div className='-mt-80 ml-1 w-full md:w-2/3 lg:w-2/3 xl:w-2/3 2xl:w-2/3 rounded-3xl bg-silver text-blue tracking-tight p-8 font-authenticSans90 text-2xl'>
+					<div className=''>
+						<span className='w-1/2'>
+							<img
+								className='w-1/3 float-left mr-4'
+								src='/pm-logo-blue.png'></img>
+						</span>
+						<p className='-mt-5'>
+							is an award-winning Los Angeles and Chicago based post-production
+							company.
+						</p>
+					</div>
+					<span className='mt-4'>
+						We specialize in sound and edit for feature films, documentaries,
+						and commercials.
+					</span>
+					{/* <p>
 								Today I spent most of the day researching ways to ...
 								<span className='inline-flex items-baseline'>
 									<img
@@ -51,8 +54,6 @@ function Home() {
 								</span>
 								keeps telling me there is no way to make it work, that ...
 							</p> */}
-						</div>
-					</div>
 				</div>
 			</div>
 		</>
