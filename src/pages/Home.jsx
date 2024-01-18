b137aff27ce703900561c2d8d21d0173645ae427
Ivan Afanasev <diykarelia@gmail.com>
2024-01-18 08:48:20 -0800

    fix: menu center

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,52 +8,47 @@ function Home() {
 		<>
 			<Menu />
 			<div className='w-screen h-screen bg-blue overflow-hidden shrink-0 m-0 p-0'>
-				<div className='flex flex-col w-full items-center'>
-					<div className='aspect-video'>
-						<Vimeo
-							video='858109861'
-							// video='902728728'
-							// muted='true'
-							background='true'
-							// controls='false'
-							height='700'
-							// width='800'
-							loop='true'
-						/>
-					</div>
-				</div>
-			</div>
-
-			<div className='bg-blue z-50 m-0 p-0 flex justify-center'>
-				{/* <div className='w-full m-0 flex flex-col md:flex-row lg:flex-row xl:flex-row 2xl:flex-row justify-center'> */}
-				<div className='-mt-80 ml-1 w-full md:w-2/3 lg:w-2/3 xl:w-2/3 2xl:w-2/3 rounded-3xl bg-silver text-blue tracking-tight p-8 font-authenticSans90 text-2xl'>
-					<div className=''>
-						<span className='w-1/2'>
-							<img
-								className='w-1/3 float-left mr-4'
-								src='/pm-logo-blue.png'></img>
-						</span>
-						<p className='-mt-5'>
-							is an award-winning Los Angeles and Chicago based post-production
-							company.
-						</p>
-					</div>
-					<span className='mt-4'>
-						We specialize in sound and edit for feature films, documentaries,
-						and commercials.
-					</span>
-					{/* <p>
-								Today I spent most of the day researching ways to ...
-								<span className='inline-flex items-baseline'>
+				<div className='bg-blue z-50 m-0 p-0 flex'>
+					<div
+						className='
+					
+					flex items-center align-center
+					'>
+						<div
+							className='ml-1 w-full md:w-2/3 lg:w-2/3 xl:w-2/3 2xl:w-2/3
+					fixed					
+					rounded-3xl bg-silver text-blue tracking-tight p-8 font-authenticSans90 text-2xl'>
+							<div className=''>
+								<span className='w-1/2'>
 									<img
-										src='/pm-logo-blue.png'
-										alt=''
-										className='self-center rounded-full mx-1'
-									/>
-									<span>Kramer</span>
+										className='w-1/3 float-left mr-4'
+										src='/pm-logo-blue.png'></img>
 								</span>
-								keeps telling me there is no way to make it work, that ...
-							</p> */}
+								<p className='-mt-5'>
+									is an award-winning Los Angeles and Chicago based
+									post-production company.
+								</p>
+							</div>
+							<span className='mt-4'>
+								We specialize in sound and edit for feature films,
+								documentaries, and commercials.
+							</span>
+						</div>
+					</div>
+					<div className='flex flex-col w-full items-center'>
+						<div className='aspect-video'>
+							<Vimeo
+								video='858109861'
+								// video='902728728'
+								// muted='true'
+								background='true'
+								// controls='false'
+								height='700'
+								// width='800'
+								loop='true'
+							/>
+						</div>
+					</div>
 				</div>
 			</div>
 		</>
