5d191d30627660f2e9e0e8014fe48185ddaf878d
Ivan Afanasev <diykarelia@gmail.com>
2024-01-18 23:53:51 -0800

    fix: about word break

--- src/pages/Person.jsx
+++ src/pages/Person.jsx
@@ -5,8 +5,8 @@ function Project({ personinfo }) {
 			<div className='overlay'></div>
 
 			<div
-				className={`overflow-hidden bg-darkyellow design pb-20 shrink-0 w-full md:shrink-0`}>
-				<div className='inline-block mt-20'>
+				className={`overflow-hidden bg-darkyellow design shrink-0 w-full md:shrink-0`}>
+				<div className='inline-block mt-10'>
 					{/* <div className='grid grid-cols-8 gap-4 ml-0 mr-0 mb-20'> */}
 					<div className='ml-8 mr-8 mb-20 grid min-[640px]:grid-cols-2'>
 						{/* Left: poster */}
@@ -26,7 +26,7 @@ function Project({ personinfo }) {
 						</div>
 						<div className='absolute'>
 							<h1
-								className={`uppercase mt-96 text-silver text-6xl min-[640px]:text-8xl break-all font-authenticSans150`}>
+								className={`uppercase mt-96 text-silver text-6xl min-[640px]:text-8xl break-word font-authenticSans150`}>
 								{personinfo.name}
 							</h1>
 						</div>
